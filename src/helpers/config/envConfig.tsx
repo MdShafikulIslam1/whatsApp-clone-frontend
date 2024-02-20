d5d7de830742479b72447d2acd6ee14e1be45dd4
shofikul <179398ssshofik@gmail.com>
2024-02-20 21:46:09 +0600

    add server link statically

--- src/helpers/config/envConfig.tsx
+++ src/helpers/config/envConfig.tsx
@@ -1,1 +1,3 @@
-export const getBaseUrl = () => process.env.NEXT_PUBLIC_API_BASE_URL as string;
+export const getBaseUrl = () =>
+  (process.env.NEXT_PUBLIC_API_BASE_URL as string) ||
+  ("https://whats-app-server.vercel.app/api/v1" as string);
